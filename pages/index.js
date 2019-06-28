7c843061cda16ee20f482ac89d94d0f8c4220af9
Kimmi Gan <kimmi.gan@onfido.com>
2019-06-28 14:49:43 +0100

    Troubleshooting SDK loading issue

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import dynamic from 'next/dynamic';
 
 // Test A
 // import { init } from 'onfido-sdk-ui';
+// const Onfido = dynamic(import ('onfido-sdk-ui'), {ssr:false});
 
 class OnfidoTest extends Component {
   // Test B
   componentDidMount () {
-    const Onfido = require('onfido-sdk-ui');
+    // const Onfido = require('onfido-sdk-ui');
+    // const Onfido = dynamic(import ('onfido-sdk-ui'), {ssr:false});
   }
 
   // Test C
-  // async componentDidMount () {
-  //   const Onfido = await import('onfido-sdk-ui');
-  // }
+  async componentDidMount () {
+    const Onfido = await dynamic(import ('onfido-sdk-ui'), {ssr:false});
+  }
   
   render () {
     return (
